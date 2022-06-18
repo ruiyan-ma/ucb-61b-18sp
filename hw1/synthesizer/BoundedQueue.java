802890eb4a9903dd07cf4fcd1f7b9e2dda88ffe1
ruiyan-ma <maruiyan17@gmail.com>
2022-06-17 14:20:34 -1000

    finish hw1

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,39 @@
+package synthesizer;
+
+public interface BoundedQueue<T> extends Iterable<T> {
+
+    /**
+     * Return size of the buffer.
+     */
+    int capacity();
+
+    /**
+     * Return number of items currently in the buffer.
+     */
+    int fillCount();
+
+    /**
+     * Add item x to the end.
+     */
+    void enqueue(T x);
+
+    /**
+     * Delete and return item from the front.
+     */
+    T dequeue();
+
+    /**
+     * Return (but do not delete) item from the front.
+     */
+    T peek();
+
+    /** Is the buffer empty? */
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    /** Is the buffer full? */
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+}
--- proj2/byog/lab5/RandomWorldDemo.java
+++ proj2/byog/lab5/RandomWorldDemo.java
@@ -11,9 +11,11 @@ import java.util.Random;
  */
 public class RandomWorldDemo {
     private static final int WIDTH = 50;
+
     private static final int HEIGHT = 50;
 
     private static final long SEED = 2873123;
+
     private static final Random RANDOM = new Random(SEED);
 
     /**
