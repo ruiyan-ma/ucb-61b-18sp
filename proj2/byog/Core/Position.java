6450cce2e79568a4a4ff0ed96e0ba05c1b113a6b
ruiyan-ma <maruiyan17@gmail.com>
2022-06-21 22:09:41 -0700

    start proj2

---
+++ proj2/byog/Core/Position.java
@@ -0,0 +1,14 @@
+package byog.Core;
+
+/**
+ * Position class to represent a coordinate.
+ */
+public class Position {
+    int x;
+    int y;
+
+    Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
